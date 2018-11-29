b5d20df76ffd008f6c0957be073715840f2df117
zhiguan7 <zhiguan7@gmail.com>
2018-11-29 11:49:38 +0800

    0.0.2

--- app/src/main/java/com/example/wind/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/example/wind/databasetest/MyDatabaseHelper.java
@@ -8,7 +8,10 @@ import android.widget.Toast;
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String CREATE_BOOK = "create table Book(" + "id integer primary key autoincrement, "
-            +"author text," + "price real," +  "pages integer," + "name text)";
+            + "author text," + "price real," +  "pages integer," + "name text)";
+
+    public static final String CREATE_CATEGORY = "create table Category (" + "id integer primary key autoincrement,"
+            + "category_name text," + "category_code integer)";
 
     private Context mContext;
 
@@ -20,11 +23,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("drop table if exists Book");
+        sqLiteDatabase.execSQL("drop table if exists Category");
+        onCreate(sqLiteDatabase);
     }
 }
