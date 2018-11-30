0a1451c5f5c4c9b8ffd850f16ddb96359af990f6
zhiguan7 <zhiguan7@gmail.com>
2018-11-30 13:17:18 +0800

    0.0.3

--- app/src/main/java/com/example/wind/databasetest/MainActivity.java
+++ app/src/main/java/com/example/wind/databasetest/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.wind.databasetest;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+
+                values.put("name", "The King");
+                values.put("author", "Wind");
+                values.put("pages", 159);
+                values.put("price", 15.50);
+                db.insert("Book", null, values);
+                values.clear();
+
+                values.put("name", "Knight");
+                values.put("author", "yLeon");
+                values.put("pages", 198);
+                values.put("price", 51.20);
+                db.insert("Book", null, values);
+                values.clear();
+                Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 40.99);
+                db.update("Book", values, "name = ?", new String[]{"The King"});
+                values.clear();
+                Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
             }
         });
     }
